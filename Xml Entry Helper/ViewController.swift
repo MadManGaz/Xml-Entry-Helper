3fae9203cabd31bec4a0e6eb54af41ad6929801c
Gary Moore <moore-g26@ulster.ac.uk>
2019-02-16 19:43:22 +0000

    Initial Commit

---
+++ Xml Entry Helper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xml Entry Helper
+//
+//  Created by Gary Moore on 16/02/2019.
+//  Copyright © 2019 Gary Moore. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Xml Entry HelperTests/Xml_Entry_HelperTests.swift
@@ -0,0 +1,34 @@
+//
+//  Xml_Entry_HelperTests.swift
+//  Xml Entry HelperTests
+//
+//  Created by Gary Moore on 16/02/2019.
+//  Copyright © 2019 Gary Moore. All rights reserved.
+//
+
+import XCTest
+@testable import Xml_Entry_Helper
+
+class Xml_Entry_HelperTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
